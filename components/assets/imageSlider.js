2126c9a19ca692632bb02d26deb140b85e2d0ebc
Vijay pandey <vijaypandey738512@gmail.com>
2025-06-24 13:32:41 +0530

    Add files via upload

---
+++ components/assets/imageSlider.js
@@ -0,0 +1,22 @@
+//src/components/sliderImage.js
+
+import First from "../assets/First.jpg";
+import Second from "../assets/Second.jpg";
+
+ export default [
+{
+    title: "First Slide",
+    description: "This is the first slider Image of our carousel",
+    urls: First,
+},
+{
+    title: "Second Slide",
+    description: "This is the second slider Image of our carousel",
+    urls: Second,
+},
+{
+    title: "Third Slide",
+    description: "This is the Third slider Image of our carousel",
+    urls: "https://mdbootstrap.com/img/Photos/Slides/img (133).jpg" ,
+},
+ ];
